13c3eda5c0f8d6609d343ff14c1015248daff1af
Rhett North <retsrawk@gmail.com>
2024-06-21 20:44:41 -0600

    HTML structure, fetch call
    In this commit, I set up the structure of the UI
    in the src HTML file, and built the async function
    for fetching the weather data from WeatherAPI.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+const key = 'd29476f814a14afebb130134242106';
+
+async function getWeather(city) {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3`, { mode: 'cors' });
+        let data = await response.json();
+        return ({
+            location: data.location.name,
+            currentIcon: data.current.condition.icon,
+            currentTempC: data.current.temp_c,
+            currentTempF: data.current.temp_f,
+            forecast: data.forecast.forecastday
+        });
+    } catch (err) {
+        console.log('fetch error:', err)
+        return null;
+    }
+}
+
+
