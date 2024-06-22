cb2415f93bde67ee205650de8426b1e7a7452f54
Rhett North <retsrawk@gmail.com>
2024-06-21 20:58:07 -0600

    Add DOM functions, loading function
    In this commit, I have fleshed out the functions
    for the loading bar, as well as for the extraction
    of specific data, and using that data in
    manipulation of the DOM.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './style.css';
 
 const key = 'd29476f814a14afebb130134242106';
+const searchBar = document.querySelector('.city-search');
+const button = document.querySelector('.city-submit');
+const weatherCards = document.querySelector('.cards');
+const mainCard = document.querySelector('.card-main');
+const cards = document.querySelectorAll('.card');
+const loading = document.querySelector('.loading');
 
 async function getWeather(city) {
     try {
@@ -9,8 +15,7 @@ async function getWeather(city) {
         return ({
             location: data.location.name,
             currentIcon: data.current.condition.icon,
-            currentTempC: data.current.temp_c,
-            currentTempF: data.current.temp_f,
+            currentTemp: data.current.temp_f,
             forecast: data.forecast.forecastday
         });
     } catch (err) {
@@ -19,4 +24,61 @@ async function getWeather(city) {
     }
 }
 
+async function submitCity(e = "") {
+    if (e) e.preventDefault();
+    turnLoading("on");
+    const data = await getWeather(searchBar.value ? searchBar.value : "phoenix");
+    displayWeather(data);
+    turnLoading("off");
+}
 
+function displayWeather(data) {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const month = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    const date = new Date();
+
+    let counter = date.getDay();
+
+    const mainCardWeek = mainCard.querySelector('.card-week');
+    const mainCardDate = mainCard.querySelector('.card-date');
+    const mainCardCity = mainCard.querySelector('.card-city');
+    const mainCardWeather = mainCard.querySelector('.card-weather span');
+    const mainCardImg = mainCard.querySelector('.card-img');
+
+    mainCardWeek.textContent = daysOfWeek[date.getDay()];
+    mainCardDate.textContent = date.getDate() + ', ' + month[date.getMonth()];
+    mainCardCity.textContent = data.locationName;
+    mainCardWeather.textContent = Math.round(data.currentTemp);
+    mainCardImg.src = 'https://' + data.currentIcon;
+
+    cards.forEach((card, i) => {
+        if (counter == 2) counter = 0;
+        else counter += 1;
+
+        const cardData = data.forecast[i + 1];
+
+        const cardWeek = card.querySelector('.card-date');
+        const cardHigh = card.querySelector('.card-weather span');
+        const cardLow = card.querySelector('.card-subweather span');
+        const cardImg = card.querySelector('#card-img');
+
+        cardWeek.textContent = daysOfWeek[counter];
+        cardHigh.textContent = Math.round(cardData.day.maxtemp_f);
+        cardLow.textContent = Math.round(cardData.day.mintemp_f);
+        cardImg.src = 'https://' + cardData.day.condition.icon;
+    });
+}
+
+function turnLoading(turn = "on") {
+    if (turn == "on") {
+        loadingBar.classList.remove('hidden');
+        weatherCards.classList.add('hidden');
+    } else {
+        loadingBar.classList.add('hidden');
+        weatherCards.classList.remove('hidden');
+    }
+}
+
+submitCity();
+
+button.addEventListener('click', submitCity);
